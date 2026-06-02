a7e5c86500418809a3cda8377d5ef457beb1483d
martingeorgiev <martog232@gmail.com>
2026-06-02 22:48:36 +0300

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/example/nabatvoting/NabatVotingApplication.java
@@ -0,0 +1,13 @@
+package com.example.nabatvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabatVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NabatVotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nabatvoting/NabatVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nabatvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NabatVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
